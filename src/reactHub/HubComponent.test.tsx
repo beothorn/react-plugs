7670ae8e7888dac30de8eb8f16b127e01f76abfe
lucass <lucass@gmail.com>
2021-04-21 23:37:34 +0200

    Fix a test and prepare next release

--- src/reactHub/HubComponent.test.tsx
+++ src/reactHub/HubComponent.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 test('Render all components', () => {
-    const component: React.FunctionComponent<{val: string}> = ({val}) => <p>{val}</p>
+    const component: React.FunctionComponent<any> = ({val}) => <p>{val}</p>
 
     const components = new Map(Object.entries({ 
         "a": component,
